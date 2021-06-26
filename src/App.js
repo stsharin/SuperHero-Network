599f65298fd90c81e7d27f67638057db096b2f68
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-06-27 01:29:21 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import NotFound from "./components/Not Found/NotFound";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path="/home">
+            <Home/>
+          </Route>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
