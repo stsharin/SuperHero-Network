c9d23f6fe35313d8f0113858ce67897fa8aba0a3
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-06-28 03:45:55 +0600

    added handleSubmit and handleDateChange

--- src/components/RegisterEvent/RegisterEvent.js
+++ src/components/RegisterEvent/RegisterEvent.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 const RegisterEvent = () => {
+
+    const handleDateChange = (e) => {
+        console.log(e);
+    }
+
+    const handleSubmit = () => {
+        
+    }
+
     return (
-        <div>
-            This is register
+        <div className="container">
+            <div className="d-flex justify-content-center">
+                <form className="form-control m-5" onSubmit={handleSubmit}>
+                    <h3>Register as a SuperHero</h3><br/>
+                    <input type="text" name="userName" placeholder="Username"></input><br/><br/>
+                    <input type="text" name="eventName" placeholder="Event Name"></input><br/><br/>
+                    <input type="text" name="description" placeholder="Description"></input><br/><br/>
+                    <input type="date" onChange={handleDateChange} name="date"></input><br/><br/>
+                    <button className="btn btn-primary">Registration</button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/TotalEvents/TotalEvents.js
+++ src/components/TotalEvents/TotalEvents.js
@@ -4,8 +4,21 @@ import Event from '../Event/Event';
 
 const TotalEvents = () => {
     // console.log(fakeData);
+
+    // will use later
+    // const addAllEvents = () => {
+    //     fetch('http://localhost:5000/addEvents', {
+    //         method: 'POST',
+    //         headers: {'Content-Type': 'application/json'},
+    //         body: JSON.stringify(eventData)
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => console.log(data));
+    // }
+
     return (
         <div className="row">
+            {/* <button onClick={addAllEvents}>Add All Event</button> */}
             {
                 // passing data to child component
                 eventData.map(e => <Event event={e}></Event>)
